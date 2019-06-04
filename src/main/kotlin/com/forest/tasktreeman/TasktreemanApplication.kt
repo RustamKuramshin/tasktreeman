610321dc33cd1f1858847a0db653318baa4c4e4a
Rustam Kuramshin <leo.in.empty@gmail.ru>
2019-06-04 22:49:29 +0300

    init
    Signed-off-by: Rustam Kuramshin <leo.in.empty@gmail.ru>

---
+++ src/main/kotlin/com/forest/tasktreeman/TasktreemanApplication.kt
@@ -0,0 +1,11 @@
+package com.forest.tasktreeman
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TasktreemanApplication
+
+fun main(args: Array<String>) {
+    runApplication<TasktreemanApplication>(*args)
+}
